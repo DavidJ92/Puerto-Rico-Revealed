2b6b6f01ee373556ae1736498ac176c1c0266ea5
DavidJ92 <141689412+DavidJ92@users.noreply.github.com>
2024-06-26 00:44:05 -0500

    fixing video bug

--- js/script.js
+++ js/script.js
@@ -2,15 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     var video = document.getElementById('video-background');
     
     // Attempt to play the video automatically
-    video.play().catch(function(error) {
-        // Autoplay was prevented, wait for user interaction
-        console.warn('Autoplay was prevented. Waiting for user interaction.');
+    var promise = video.play();
 
-        // Add a click event listener to start playback on user interaction
-        document.addEventListener('click', function() {
-            if (video.paused) {
-                video.play();
-            }
+    if (promise !== undefined) {
+        promise.then(function() {
+            // Autoplay started successfully
+            console.log('Autoplay started');
+        }).catch(function(error) {
+            // Autoplay was prevented, wait for user interaction
+            console.warn('Autoplay was prevented. Waiting for user interaction.');
+            
+            // Add a click event listener to start playback on user interaction
+            document.addEventListener('click', function() {
+                if (video.paused) {
+                    video.play();
+                }
+            });
         });
-    });
+    }
 });
